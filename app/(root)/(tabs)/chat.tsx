ca9996c16e9d099c2b2363bb754c2af6487c0b76
mycodedlife <ifiokusanga69@gmail.com>
2024-09-17 10:46:54 +0100

    onboarding-screens

---
+++ app/(root)/(tabs)/chat.tsx
@@ -0,0 +1,13 @@
+import { Text, StyleSheet } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context";
+
+
+const Chat = () => {
+    return (
+        <SafeAreaView>
+            <Text>Home</Text>
+        </SafeAreaView>
+    );
+};
+
+export default Chat;
---
+++ app/(root)/(tabs)/profile.tsx
@@ -0,0 +1,13 @@
+import { Text, StyleSheet } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context";
+
+
+const Profile = () => {
+    return (
+        <SafeAreaView>
+            <Text>Home</Text>
+        </SafeAreaView>
+    );
+};
+
+export default Profile;
